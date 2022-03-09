8dcb2b9c9efc996af71ce9f69dce674700634383
Paul Leroy <paul.leroy@univ-rennes1.fr>
2022-03-09 11:46:38 +0100

    denoising_with_lastools

--- denoising_with_lastools.py
+++ denoising_with_lastools.py
@@ -5,7 +5,19 @@ Created on Thu Mar  3 09:07:15 2022
 @author: PaulLeroy
 """
 
+import multiprocessing
+import os
+
 import lastools
 
-lastools.lasgrid(
-    , step)
+idir = r'C:\DATA\Brioude_30092021\05-Traitements\C2'
+i = os.path.join(idir, "*.laz")
+odir = r'C:\DATA\Brioude_30092021\05-Traitements\C2\denoised'
+step = 10
+isolated = 20
+
+Ncores = multiprocessing.cpu_count()
+cores = int(Ncores / 2)
+
+# %% lasnoise step 10 isolated 20 (or 25) remove_noisecore 50 odix _1 olaz
+lastools.lasnoise(i, odir, step=step, isolated=isolated, cores=cores)
