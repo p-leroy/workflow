1b763dcb11450659807e563ce604b652f6a8d7f1
Paul Leroy <paul.leroy@univ-rennes1.fr>
2022-05-25 11:50:01 +0200

    assembly_plan code refactored
    One file with from_tiles and from_lines functions

--- script_assembly_plan.py
+++ script_assembly_plan.py
@@ -2,8 +2,8 @@ import glob, os
 
 import assembly_plan as assembly_plan
 
-idir = r'C:\DATA\Brioude_30092021\05-Traitements\tmp'
+idir = r'C:\DATA\Brioude_30092021\05-Traitements\C3_denoised_i'
 
 lines = glob.glob(os.path.join(idir, "*.laz"))
 
-assembly_plan.create(lines, idir)
+assembly_plan.from_lines(lines, idir)
